b73c40b55da170af807708b111970babdc391464
mayflowerq <82655677+mayflowerq@users.noreply.github.com>
2022-12-12 21:58:01 +0300

    Update issue_05.py

--- HW_class_8/issue_05.py
+++ HW_class_8/issue_05.py
@@ -6,7 +6,6 @@ class Color:
 	def __init__(self, red: int = 0, green: int = 0, blue: int = 0):
 		if not (0 <= red <= 255 and 0 <= green <= 255 and 0 <= blue <= 255):
 			raise ValueError('Incorrect input')
-
 		self.rgb = (red, green, blue)
 
 	def __repr__(self):
@@ -22,7 +21,8 @@ class Color:
 		return self.rgb == other.rgb
 
 	def __add__(self, other):
-		return Color(min(255, self.rgb[0] + other.rgb[0]), min(255, self.rgb[1] + other.rgb[1]), min(255, self.rgb[2] + other.rgb[2]))
+		return Color(min(255, self.rgb[0] + other.rgb[0]), min(255, self.rgb[1] +
+								other.rgb[1]), min(255, self.rgb[2] + other.rgb[2]))
 
 	def __hash__(self):
 		return hash(self.rgb)
@@ -31,10 +31,12 @@ class Color:
 		if not 0 <= c <= 1:
 			raise ValueError('Contrast must be between 0.0 and 1.0.')
 		cl = 256 * (c-1)
-		F = 259* (cl + 255) / 255 / (259 - cl)
+		F = 259 * (cl + 255) / 255 / (259 - cl)
 		return Color(*(int(F * (color - 128)) + 128 for color in self.rgb))
+
 	def __rmul__(self, c: float):
 		return self.__mul__(c)
+
 
 if __name__ == '__main__':
 	orange1 = Color(255, 165, 0)
