e86bb775bf1855ccfd5639259076bb9815ec1bc1
mayflowerq <82655677+mayflowerq@users.noreply.github.com>
2022-12-12 21:56:39 +0300

    Update issue_04.py

--- HW_class_8/issue_04.py
+++ HW_class_8/issue_04.py
@@ -6,10 +6,9 @@ class Color:
 	def __init__(self, red: int = 0, green: int = 0, blue: int = 0):
 		if not (0 <= red <= 255 and 0 <= green <= 255 and 0 <= blue <= 255):
 			raise ValueError('Incorrect input')
+		self.rgb = [red, green, blue]
 
-		self.rgb = (red, green, blue)
-
-	def __repr__(self):
+	def __str__(self):
 		return (
 			f'{self.START};{self.rgb[0]};{self.rgb[1]};{self.rgb[2]}{self.MOD}'
 			f'●'
@@ -22,15 +21,13 @@ class Color:
 		return self.rgb == other.rgb
 
 	def __add__(self, other):
-		return Color(min(255, self.rgb[0] + other.rgb[0]), min(255, self.rgb[1] + other.rgb[1]), min(255, self.rgb[2] + other.rgb[2]))
+		return Color(min(255, self.rgb[0] + other.rgb[0]), min(255, self.rgb[1] +
+							other.rgb[1]), min(255, self.rgb[2] + other.rgb[2]))
 
-	def __hash__(self):
-		return hash(self.rgb)
 
 if __name__ == '__main__':
-	orange1 = Color(255, 165, 0)
-	red = Color(255, 0, 0)
-	green = Color(0, 255, 0)
-	orange2 = Color(255, 165, 0)
-	color_list = [orange1, red, green, orange2]
-	print(set(color_list))
+	redd = Color(0, 255, 0)
+	print(redd)
+	print(redd == Color(0, 255, 0))
+	print(redd == redd)
+	print(Color(0, 255, 0) + Color(255, 0, 0))
